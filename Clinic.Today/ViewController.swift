5d520c070e7a4f226effd0928511a1c13397cc28
Ruben Martinez Jr <rmartin@bowdoin.edu>
2014-08-07 10:34:03 -0500

    Initial Commit

---
+++ Clinic.Today/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clinic.Today
+//
+//  Created by Ruben on 8/7/14.
+//  Copyright (c) 2014 Ruben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
